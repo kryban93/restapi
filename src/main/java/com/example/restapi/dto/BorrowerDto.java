4878e76259e08bf8d9ef4d8d178f92721a6d6367
kryban93 <kryban1993@gmail.com>
2023-02-23 14:04:49 +0100

    Fixes for users

--- src/main/java/com/example/restapi/dto/BorrowerDto.java
+++ src/main/java/com/example/restapi/dto/BorrowerDto.java
@@ -2,6 +2,7 @@ package com.example.restapi.dto;
 
 import com.example.restapi.model.BorrowerEntity;
 import lombok.Data;
+import org.jetbrains.annotations.NotNull;
 
 @Data
 public class BorrowerDto {
@@ -9,7 +10,7 @@ public class BorrowerDto {
     private String name;
     private String lastname;
 
-    public BorrowerDto(BorrowerEntity borrower) {
+    public BorrowerDto(@NotNull BorrowerEntity borrower) {
         this.id = borrower.getId();
         this.name = borrower.getName();
         this.lastname = borrower.getLastname();
