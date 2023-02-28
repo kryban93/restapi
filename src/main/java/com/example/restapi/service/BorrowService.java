3f27558904f1c918a032b000ba72e96ceaf22b1a
kryban93 <kryban1993@gmail.com>
2023-02-28 17:32:57 +0100

    updated borrow service

--- src/main/java/com/example/restapi/service/BorrowService.java
+++ src/main/java/com/example/restapi/service/BorrowService.java
@@ -1,21 +1,15 @@
 package com.example.restapi.service;
 
-import com.example.restapi.dto.BookDto;
 import com.example.restapi.dto.BorrowerDto;
 import com.example.restapi.model.BookEntity;
 import com.example.restapi.model.BorrowerEntity;
 import com.example.restapi.model.Borrows;
 import com.example.restapi.repository.BookRepository;
-import com.example.restapi.repository.BorrowersRepository;
-import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
+import com.example.restapi.repository.BorrowersRepository;import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Slf4j
 @Service
@@ -30,14 +24,13 @@ public class BorrowService {
 
 
     public BorrowerDto borrowBook(Borrows borrows) {
-        log.info(String.valueOf(borrows.getBookid()));
+
         Optional<BookEntity> bookOptional = bookRepository.findById(borrows.getBookid());
         Optional<BorrowerEntity> borrowerOptional = borrowersRepository.findById(borrows.getBorrowerid());
         if (bookOptional.isPresent() && borrowerOptional.isPresent()) {
             BorrowerEntity borrowerEntity =  borrowerOptional.get();
             BookEntity bookEntity = bookOptional.get();
-            log.info(String.valueOf(borrowerEntity));
-            log.info(String.valueOf(bookEntity));
+
             List<BookEntity> userBooks = borrowerEntity.getBooks();
             userBooks.add(bookEntity);
             borrowerEntity.setBooks(userBooks);
